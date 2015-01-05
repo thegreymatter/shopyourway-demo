b7f9f8d0ce610ae774a371cb007518fa5519b74d
Dave Mullins <DaveMullinsJr@gmail.com>
2015-01-05 11:05:11 -0500

    wip syw api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -53,6 +53,7 @@ class WelcomeController < ApplicationController
   def proxy
     response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=toy&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
     @response = response
+    render json: @response
     # @products = response['products']
   end
   def testing
