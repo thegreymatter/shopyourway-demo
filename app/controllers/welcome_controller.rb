176a86dcfa534a05b32e98774f4b90d863569376
Dave Mullins <DaveMullinsJr@gmail.com>
2015-01-05 10:22:22 -0500

    wip syw api json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -31,8 +31,10 @@ class WelcomeController < ApplicationController
 
     # Reading response from app proxy page
     response = HTTParty.get('https://hapyak-shopyourway.herokuapp.com/proxy')
-    binding.pry
-    @products = response['products']
+    @response = response.to_json
+
+    # binding.pry
+    # @products = response['products']
 
     # Shopyourway User Profile Request
     user_profile_base_url = "http://sandboxplatform.shopyourway.com/users/get?token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb&ids="
@@ -49,6 +51,7 @@ class WelcomeController < ApplicationController
   end
   def proxy
     response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=toy&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
-    @products = response['products']
+    @response = response
+    # @products = response['products']
   end
 end
