16fcae81e068d1adf83f6d9e994421bb8fe68e6f
Dave Mullins <DaveMullinsJr@gmail.com>
2015-01-05 09:51:36 -0500

    wip proxy page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -29,8 +29,9 @@ class WelcomeController < ApplicationController
       syw_user_id = false
     end
 
-    # Shopyourway Product Request
-    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=toy&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
+    # Reading response from app proxy page
+    response = HTTParty.get('https://hapyak-shopyourway.herokuapp.com/proxy')
+    binding.pry
     @products = response['products']
 
     # Shopyourway User Profile Request
