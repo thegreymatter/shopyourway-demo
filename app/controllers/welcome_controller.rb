19029194cff8aa93ae48b7ee4617fd42e3a8374b
Dave Mullins <DaveMullinsJr@gmail.com>
2015-01-06 10:04:44 -0500

    final push to setup api request on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,17 +8,14 @@ class WelcomeController < ApplicationController
   end
 
   def index
-
     # Javascript request from custom proxy page
-    response = HTTParty.get('https://hapyak-shopyourway.herokuapp.com/proxy')
-    # binding.pry
-    @products = response.shuffle.take(8)
+    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=toy&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
+    @products = response['products'].shuffle.take(8)
 
     @user_profile_name = false
     @user_profile_image = false
 
   end
-
   def custom
     # How to pass params through the url
     # ?hapyak_username=Dave&hapyak_SYWID=5696025
@@ -80,7 +77,6 @@ class WelcomeController < ApplicationController
     end
 
     response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=toy&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
-    # response = HTTParty.get('https://hapyak-shopyourway.herokuapp.com/proxy')
     @products = response['products']
 
     @products = convertToString @products
