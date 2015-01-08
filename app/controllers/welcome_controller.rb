f637c29b68c108a4a39008e62727e09c66d9afae
Dave Mullins <DaveMullinsJr@gmail.com>
2015-01-08 11:42:37 -0500

    reset search params

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,7 @@ class WelcomeController < ApplicationController
 
   def index
     # Javascript request from custom proxy page
-    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=toy&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
+    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=a&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
     @products = response['products'].shuffle.take(8)
 
     @user_profile_name = false
@@ -36,7 +36,7 @@ class WelcomeController < ApplicationController
     end
 
     # Reading response from app proxy page
-    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=toy&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
+    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=game&orderBy=top_sellers&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
     @products = response['products']
 
     # Shopyourway User Profile Request
@@ -77,7 +77,7 @@ class WelcomeController < ApplicationController
       end
     end
 
-    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=toy&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
+    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=game&orderBy=top_sellers&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
     @products = response['products']
 
     @products = convertToString @products
