7e5ee91452fe8cf40b5e50a758a4fc984142e2d4
Dave Mullins <DaveMullinsJr@gmail.com>
2015-01-08 11:49:05 -0500

    reset search params

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -57,17 +57,14 @@ class WelcomeController < ApplicationController
 
       if obj.is_a?(String)
 
-        # binding.pry
         obj.to_s
 
       elsif obj.is_a?(Array)
 
-        # binding.pry
         obj.map {|value| convertToString value }
 
       elsif obj.is_a?(Hash)
 
-        # binding.pry
         obj.merge( obj ) {|key, value| convertToString value }
 
       else
@@ -77,7 +74,7 @@ class WelcomeController < ApplicationController
       end
     end
 
-    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=movie&orderBy=top_sellers&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
+    response = HTTParty.get('http://sandboxplatform.shopyourway.com/products/search?q=toys&orderBy=top_sellers&token=0_18385_253402300799_1_07eabcc614049e8a68de05a41c88d0cffb5868c72da9ae0d0e290c7d34396a31&hash=9975bfc470398e9301fe654bcd386e035110469fbaf01e2a31f87e413b73abbb')
     @products = response['products']
 
     @products = convertToString @products
